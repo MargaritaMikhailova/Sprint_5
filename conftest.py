f9a9a3f704c6b596c441b2fc8cbe90d48643bd04
Margarita Mikhailova <chanel660@yandex.ru>
2026-03-27 15:56:53 +0400

    Add file conftest.py on develop branch

---
+++ conftest.py
@@ -0,0 +1,26 @@
+import random
+import time
+import pytest
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+@pytest.fixture
+def driver():
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome()
+    driver.get("https://qa-desk.stand.praktikum-services.ru/")
+
+    yield driver
+
+    driver.quit()
+
+@pytest.fixture
+def generate_email():
+
+    import random
+    return f"user{random.randint(1000, 9999)}@test.ru"
+
